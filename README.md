5cb1b9194b07876a224372c85a6316d1ee3a7a85
shekhfarid <shekhfarid1974@gmail.com>
2023-11-25 23:23:15 +0600

    first ims

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simplified-Custom-Inventory-Management-System-IMS-
+Laravel Task
+
+1.First Clone this project
+2.create database database connection
+3.php artisan config:cache
+4.php artisan key:generate
+5.php artisan migrate
+6.php artisan serve
+7.click login 
+8.email: farid@gmail.com password:11111111
+9.you will see dashboard click product page
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit a Product</h1>
+    <div>
+        @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>    
+            @endforeach
+        </ul>
+            
+        @endif
+    </div>
+    <form method="post" action="{{ route('product.update', ['product' => $product]) }}">
+        @csrf
+        @method('put')
+        <div>
+            <label>Name</label>
+            <input type="name" name="name" placeholder="name" value="{{ $product->name }}"/>
+        </div>
+        <div>
+            <label>Quantity</label>
+            <input type="quantity" name="quantity" placeholder="quantity" value="{{ $product->quantity }}"/>
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="price" name="price" placeholder="price" value="{{ $product->price }}"/>
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="description" name="description" placeholder="description" value="{{ $product->description }}"/>
+        </div>
+        <div>
+            <input type="submit" value="Update" />
+        </div>
+    </form>
+</body>
+</html>
